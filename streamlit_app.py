267d48170355f23c9b5f3ce4f610aaab61129c40
Jason Stewart <jason.stewart@gmail.com>
2022-12-17 00:59:25 -0500

    query sf and display result as df

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,21 +14,22 @@ def get_coordinates():
     WHERE ST_DWITHIN(ST_POINT({focus_coordinates[0]},{focus_coordinates[1]}),COORDINATES,{distance});
     """    
     
-    #     # connect to snowflake
-    #     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    #     my_cur = my_cnx.cursor()
+    # connect to snowflake
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
 
-    #     # run a snowflake query and put it all in a var called my_catalog
-    #     my_cur.execute("select color_or_style from catalog_for_website")
-    #     my_catalog = my_cur.fetchall()
+    # run a snowflake query and put it all in a var called my_catalog
+    my_cur.execute("select color_or_style from catalog_for_website")
+    my_catalog = my_cur.fetchall()
 
-    #     # put the dafta into a dataframe
-    #     df = pandas.DataFrame(my_catalog)
+    # put the dafta into a dataframe
+    sf_return_df = pandas.DataFrame(my_catalog)
 
     df = pd.DataFrame(
         np.random.randn(1000, 2) / [50, 50] + [40.72143702499928,-74.00296211242676],
         columns=['lat', 'lon'])
-
     st.map(df)
+    
+    st.dataframe(sf_return_df)
 
 get_coordinates()
